003f9a1329e5e337ee471a708c0c2b3b139b0104
Paul Brumleve <paul.brumleve@gmail.com>
2022-03-25 23:05:00 -0500

    Create woring game() funstion that plays 'Rock, Paper, Scissors best of 5 games.

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 function game() {
-  let playerWins;
-  let computerWins;
-  for (playerWins = 0, computerWins = 0; (playerWins + computerWins) < 5; ) { 
+  let playerWinCount;
+  let computerWinCount;
+  for (playerWinCount = 0, computerWinCount = 0; (playerWinCount + computerWinCount) < 5; ) { 
     const playerSelection = capitalize(prompt('Choose one:   Rock    Paper   Scissors', '')); 
     const computerSelection = computerPlay();
-    function playRound(playerSelection, computerSelection) {
-      if (playerSelection === computerSelection) {
-        return `You tied! Replay the round.`;
-      } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
-                 (playerSelection === 'Paper' && computerSelection === 'Rock') || 
-                 (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-      } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || 
-                 (playerSelection === 'Paper' && computerSelection === 'Scissors') || 
-                 (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    playRound(playerSelection, computerSelection);
+    console.log(`You played ${playerSelection} \nThe computer played ${computerSelection} \n${playRound(playerSelection, computerSelection)}`);
+    if (playRound(playerSelection, computerSelection) === `You Win! ${playerSelection} beats ${computerSelection}`) 
+    {
+      if (checkEndGame(playerWinCount)=== true) 
+      {
+        return console.log(`Congratulations! You Won!`);
       } else {
-        return `Invalid entry. You must enter Rock, Paper, or Scissors. Try again!`;
+        playerWinCount++;
+        console.log(`The score is ${playerWinCount} - ${computerWinCount}. \nBest of 5. Go again`);
       }
-    }
-    if (playRound(playerSelection, computerSelection) === `You Win! ${playerSelection} beats ${computerSelection}`) {
-      playerWins++;
-      console.log(`You played ${playerSelection} \nThe computer played ${computerSelection} \n${playRound(playerSelection, computerSelection)} \nThe score is ${playerWins} - ${computerWins}. \nBest of 5. Go again`);
     } else if (playRound(playerSelection, computerSelection) === `You Lose! ${computerSelection} beats ${playerSelection}`) {
-      computerWins++;
-      console.log(`You played ${playerSelection} \nThe computer played ${computerSelection} \n${playRound(playerSelection, computerSelection)} \nThe score is ${playerWins} - ${computerWins}. \nBest of 5. Go again`);
-    } else{
-      console.log(`You played ${playerSelection} \nThe computer played ${computerSelection} \n${playRound(playerSelection, computerSelection)} \nThe score is ${playerWins} - ${computerWins}. \nBest of 5. Go again`);
+      if (checkEndGame(computerWinCount)) {
+        return console.log(`You lost the game. Better luck next time!`);
+      } else {
+        computerWinCount++;
+        console.log(`The score is ${playerWinCount} - ${computerWinCount}. \nBest of 5. Go again`);
+      }
+    } else {
+      console.log(`The score is ${playerWinCount} - ${computerWinCount}. \nBest of 5. Go again`);
     }
   }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `You tied! Replay the round.`;
+  } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+             (playerSelection === 'Paper' && computerSelection === 'Rock') || 
+             (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || 
+             (playerSelection === 'Paper' && computerSelection === 'Scissors') || 
+             (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `Invalid entry. You must enter Rock, Paper, or Scissors. Try again!`;
+  }
+}
+
+function checkEndGame(winCount) {
+  return winCount === 2;
 }
 
 function capitalize(str) {
