7a7b8acf46cf7b624189a1988188f07dd2d74400
Paul Brumleve <paul.brumleve@gmail.com>
2022-09-21 21:40:46 -0500

    added win condition to game.

--- script.js
+++ script.js
@@ -9,56 +9,60 @@ buttons.forEach((button) => {
 })
 
 function playRound(playerSelection, computerSelection) {
-  computerSelection = computerPlay();
-  playerSelection = event.target.id;
-  if (playerSelection === computerSelection) {
-    return console.log(`You played ${playerSelection} \n
-                        The computer played ${computerSelection} \n
-                        The score is ${playerWinCount} - ${computerWinCount}. \n
-                        You tied! Replay the round.`);
-  } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
-             (playerSelection === 'Paper' && computerSelection === 'Rock') || 
-             (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-    playerWinCount++;
-    if (checkEndGame(playerWinCount) === true) {
-      document.querySelectorAll('button').disabled = true;
-      return console.log(`You played ${playerSelection} \n
-                          The computer played ${computerSelection} \n
-                          You Win! ${computerSelection} beats ${playerSelection} \n 
-                          The score is ${playerWinCount} - ${computerWinCount}. \n
-                          Congratulations! You Won!`);
-    } else {
-      return console.log(`You played ${playerSelection} \n
-                          The computer played ${computerSelection} \n
-                          You Win! ${playerSelection} beats ${computerSelection} \n
-                          The score is ${playerWinCount} - ${computerWinCount}. \n
-                          Best of 5. Go again`);
-    }
-  } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || 
-             (playerSelection === 'Paper' && computerSelection === 'Scissors') || 
-             (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-    computerWinCount++;
-    if (checkEndGame(computerWinCount) === true) {
-      document.querySelectorAll('button').disabled = true;
-      return console.log(`You played ${playerSelection} \n
-                          The computer played ${computerSelection} \n
-                          You Lose! ${computerSelection} beats ${playerSelection} \n 
-                          The score is ${playerWinCount} - ${computerWinCount}. \n
-                          You lost the game. Better luck next time!`);
-    } else {
-      return console.log(`You played ${playerSelection} \n
-                          The computer played ${computerSelection} \n
-                          You Lose! ${computerSelection} beats ${playerSelection} \n 
-                          The score is ${playerWinCount} - ${computerWinCount}. \n
-                          Best of 5. Go again`);
-    }
+    computerSelection = computerPlay();
+    playerSelection = event.target.id;
+  if (checkEndGame(playerWinCount, computerWinCount)) {
+      return;
   } else {
-    return `Error. You must click Rock, Paper, or Scissors. Try again!`;
+    if (playerSelection === computerSelection) {
+      return console.log(`You played ${playerSelection} \n
+                          The computer played ${computerSelection} \n
+                          The score is ${playerWinCount} - ${computerWinCount}. \n
+                          You tied! Replay the round.`);
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+               (playerSelection === 'Paper' && computerSelection === 'Rock') || 
+               (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+      playerWinCount++;
+      if (checkEndGame(playerWinCount) === true) {
+        document.querySelectorAll('button').disabled = true;
+        return console.log(`You played ${playerSelection} \n
+                            The computer played ${computerSelection} \n
+                            You Win! ${computerSelection} beats ${playerSelection} \n 
+                            The score is ${playerWinCount} - ${computerWinCount}. \n
+                            Congratulations! You Won!`);
+      } else {
+        return console.log(`You played ${playerSelection} \n
+                            The computer played ${computerSelection} \n
+                            You Win! ${playerSelection} beats ${computerSelection} \n
+                            The score is ${playerWinCount} - ${computerWinCount}. \n
+                            Best of 5. Go again`);
+      }
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || 
+               (playerSelection === 'Paper' && computerSelection === 'Scissors') || 
+               (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+      computerWinCount++;
+      if (checkEndGame(computerWinCount) === true) {
+        document.querySelectorAll('button').disabled = true;
+        return console.log(`You played ${playerSelection} \n
+                            The computer played ${computerSelection} \n
+                            You Lose! ${computerSelection} beats ${playerSelection} \n 
+                            The score is ${playerWinCount} - ${computerWinCount}. \n
+                            You lost the game. Better luck next time!`);
+      } else {
+        return console.log(`You played ${playerSelection} \n
+                            The computer played ${computerSelection} \n
+                            You Lose! ${computerSelection} beats ${playerSelection} \n 
+                            The score is ${playerWinCount} - ${computerWinCount}. \n
+                            Best of 5. Go again`);
+      }
+    } else {
+      return `Error. You must click Rock, Paper, or Scissors. Try again!`;
+    }
   }
-}
+} 
 
 function computerPlay() {
-  let randomInt = Math.floor(Math.random() * 3);
+    let randomInt = Math.floor(Math.random() * 3);
   if (randomInt === 0) {
     return 'Rock';
   } else if (randomInt === 1) {
@@ -68,8 +72,8 @@ function computerPlay() {
   }
 }
 
-function checkEndGame(winCount) {
-  return winCount === 3;
+function checkEndGame(playerWinCount, computerWinCount) {
+  return playerWinCount === 3 || computerWinCount ===3;
 }
 
 function capitalize(str) {
