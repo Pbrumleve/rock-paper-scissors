6d2ba0b2ff47666d5c5734c79eab2352e8cbea6d
Paul Brumleve <paul.brumleve@gmail.com>
2022-09-21 21:32:14 -0500

    Added html buttons that when clicked play rounds of 'Rock, Paper, Scissors' with the computer in the console. No end to the game yet

--- script.js
+++ script.js
@@ -1,51 +1,75 @@
-function game() {
-  let playerWinCount;
-  let computerWinCount;
-  for (playerWinCount = 0, computerWinCount = 0; (playerWinCount + computerWinCount) < 5; ) { 
-    const playerSelection = capitalize(prompt('Choose one:   Rock    Paper   Scissors', '')); 
-    const computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    console.log(`You played ${playerSelection} \nThe computer played ${computerSelection} \n${playRound(playerSelection, computerSelection)}`);
-    if (playRound(playerSelection, computerSelection) === `You Win! ${playerSelection} beats ${computerSelection}`) 
-    {
-      if (checkEndGame(playerWinCount)=== true) 
-      {
-        return console.log(`Congratulations! You Won!`);
-      } else {
-        playerWinCount++;
-        console.log(`The score is ${playerWinCount} - ${computerWinCount}. \nBest of 5. Go again`);
-      }
-    } else if (playRound(playerSelection, computerSelection) === `You Lose! ${computerSelection} beats ${playerSelection}`) {
-      if (checkEndGame(computerWinCount)) {
-        return console.log(`You lost the game. Better luck next time!`);
-      } else {
-        computerWinCount++;
-        console.log(`The score is ${playerWinCount} - ${computerWinCount}. \nBest of 5. Go again`);
-      }
-    } else {
-      console.log(`The score is ${playerWinCount} - ${computerWinCount}. \nBest of 5. Go again`);
-    }
-  }
-}
+let playerWinCount = 0;
+let computerWinCount = 0;
+let playerSelection;
+let computerSelection;
+const buttons = document.querySelectorAll('button'); 
+
+buttons.forEach((button) => {
+  button.addEventListener('click', playRound);
+})
 
 function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
+  playerSelection = event.target.id;
   if (playerSelection === computerSelection) {
-    return `You tied! Replay the round.`;
+    return console.log(`You played ${playerSelection} \n
+                        The computer played ${computerSelection} \n
+                        The score is ${playerWinCount} - ${computerWinCount}. \n
+                        You tied! Replay the round.`);
   } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
              (playerSelection === 'Paper' && computerSelection === 'Rock') || 
              (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerWinCount++;
+    if (checkEndGame(playerWinCount) === true) {
+      document.querySelectorAll('button').disabled = true;
+      return console.log(`You played ${playerSelection} \n
+                          The computer played ${computerSelection} \n
+                          You Win! ${computerSelection} beats ${playerSelection} \n 
+                          The score is ${playerWinCount} - ${computerWinCount}. \n
+                          Congratulations! You Won!`);
+    } else {
+      return console.log(`You played ${playerSelection} \n
+                          The computer played ${computerSelection} \n
+                          You Win! ${playerSelection} beats ${computerSelection} \n
+                          The score is ${playerWinCount} - ${computerWinCount}. \n
+                          Best of 5. Go again`);
+    }
   } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || 
              (playerSelection === 'Paper' && computerSelection === 'Scissors') || 
              (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerWinCount++;
+    if (checkEndGame(computerWinCount) === true) {
+      document.querySelectorAll('button').disabled = true;
+      return console.log(`You played ${playerSelection} \n
+                          The computer played ${computerSelection} \n
+                          You Lose! ${computerSelection} beats ${playerSelection} \n 
+                          The score is ${playerWinCount} - ${computerWinCount}. \n
+                          You lost the game. Better luck next time!`);
+    } else {
+      return console.log(`You played ${playerSelection} \n
+                          The computer played ${computerSelection} \n
+                          You Lose! ${computerSelection} beats ${playerSelection} \n 
+                          The score is ${playerWinCount} - ${computerWinCount}. \n
+                          Best of 5. Go again`);
+    }
   } else {
-    return `Invalid entry. You must enter Rock, Paper, or Scissors. Try again!`;
+    return `Error. You must click Rock, Paper, or Scissors. Try again!`;
+  }
+}
+
+function computerPlay() {
+  let randomInt = Math.floor(Math.random() * 3);
+  if (randomInt === 0) {
+    return 'Rock';
+  } else if (randomInt === 1) {
+    return 'Paper';
+  } else {
+    return 'Scissors';
   }
 }
 
 function checkEndGame(winCount) {
-  return winCount === 2;
+  return winCount === 3;
 }
 
 function capitalize(str) {
@@ -55,15 +79,5 @@ function capitalize(str) {
     let firstLetterCap = str.charAt(0).toUpperCase();
     let lowerCase = str.substr(1).toLowerCase();
     return firstLetterCap + lowerCase;
-  }
-}
-function computerPlay() {
-  let randomInt = Math.floor(Math.random() * 3);
-  if (randomInt === 0) {
-    return 'Rock';
-  } else if (randomInt === 1) {
-    return 'Paper';
-  } else {
-    return 'Scissors';
   }
 }
